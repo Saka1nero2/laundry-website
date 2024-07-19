7f263c4b40e01e3af209785c07d029d2e4933a52
Script-Craftr <ennbadger@st.ug.edu.gh>
2024-07-19 01:55:20 -1200

    defined user model

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+const {mongoose} = require("mongoose")
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+    console.log("Database Connected")
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is listening`)
+    })    
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+
+const app = express()
+
